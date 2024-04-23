da12491d3e07aa6e075e2f27e2be08efb8155f7d
99-WSJ <sjwang_er@163.com>
2024-04-23 23:31:16 +0800

    4.23

---
+++ Src/src/main/java/leetcodeLearn/hot/hot22.java
@@ -0,0 +1,54 @@
+package leetcodeLearn.hot;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author wsj
+ * @description 160 相交链表
+ * @date 2024年04月22日 17:32
+ */
+public class hot22 {
+    public static void main(String[] args) {
+        ListNode node6 = new ListNode(6);
+        ListNode node5 = new ListNode(5, node6);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        ListNode node12 = new ListNode(12, node4);
+        ListNode node11 = new ListNode(11, node12);
+
+        ListNode node = getIntersectionNode(node11, node1);
+        System.out.println(node.val);
+
+
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
+
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
