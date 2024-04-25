abc17395320cc15b7dbf03d4fbe2d3164ad3117d
99-WSJ <sjwang_er@163.com>
2024-04-25 23:37:55 +0800

    4.25

--- Src/src/main/java/leetcodeLearn/hot/hot11.java
+++ Src/src/main/java/leetcodeLearn/hot/hot11.java
@@ -35,10 +35,14 @@ public class hot11 {
         // 前缀和
         int n = nums.length;
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] pair1, int[] pair2) {
-                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            @Override
+            public int compare(int[] p1, int[] p2) {
+                return p1[0] != p2[0] ? p2[0] - p1[0] : p2[1] - p1[1];
             }
+
+
         });
+
         for (int i = 0; i < k; ++i) {
             pq.offer(new int[]{nums[i], i});
         }
---
+++ Src/src/main/java/leetcodeLearn/hot/hot28.java
@@ -0,0 +1,88 @@
+package leetcodeLearn.hot;
+
+/**
+ * @author wsj
+ * @description 2、两数相加
+ * @date 2024年04月25日 21:55
+ */
+public class hot28 {
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(9);
+        ListNode node4 = new ListNode(9, node5);
+        ListNode node3 = new ListNode(2, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode head = new ListNode(1, node2);
+
+        ListNode node00 = new ListNode(9);
+        ListNode node03 = new ListNode(7);
+        ListNode node07 = new ListNode(7);
+        node00.next = node03;
+        node03.next = node07;
+
+        ListNode node = addTwoNumbers2(node00, head);
+        System.out.println(node);
+
+    }
+
+    //    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode pre = new ListNode(0);
+//        ListNode q = pre;
+//        int flag = 0;
+//        while(l1 != null && l2 != null) {
+//            int add = l1.val + l2.val + flag;
+//            q.next = new ListNode (add % 10);
+//            flag = add > 9 ? 1:0;
+//            l1 = l1.next;
+//            l2 = l2.next;
+//            q = q.next;
+//        }
+//        if(l1 == null) {
+//            while(l2 != null) {
+//                int add = flag + l2.val;
+//                q.next = new ListNode (add % 10);
+//                flag = add > 9 ? 1:0;
+//                l2 = l2.next;
+//                q = q.next;
+//            }
+//            if(flag == 1 ) {
+//                q.next = new ListNode (1);
+//            }
+//            return pre.next;
+//        }
+//
+//        while(l1 != null) {
+//            int add = flag + l1.val;
+//            q.next = new ListNode (add % 10);
+//            flag = add > 9 ? 1:0;
+//            l1 = l1.next;
+//            q = q.next;
+//        }
+//        if(flag == 1 ) {
+//            q.next = new ListNode (1);
+//        }
+//        return pre.next;
+//    }
+    static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode q = pre;
+        int flag = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null? 0: l1.val;
+            int n2 = l2 == null? 0: l2.val;
+            int add = n1 + n2 + flag;
+            q.next = new ListNode(add % 10);
+            flag = add > 9 ? 1 : 0;
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                l2 = l2.next;
+            }
+            q = q.next;
+        }
+        if (flag == 1) {
+            q.next = new ListNode(1);
+        }
+        return pre.next;
+    }
+}
