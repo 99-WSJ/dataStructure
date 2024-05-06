d478f9ef79309b7a6e0ee7e3a6311941357e7155
99-WSJ <sjwang_er@163.com>
2024-05-06 23:44:27 +0800

    5.6  1

--- Src/src/main/java/leetcodeLearn/hot/hot18.java
+++ Src/src/main/java/leetcodeLearn/hot/hot18.java
@@ -1,9 +1,53 @@
 package leetcodeLearn.hot;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author wsj
- * @description
+ * @description 73 矩阵置零
  * @date 2024年04月27日 20:56
  */
 public class hot18 {
+    public static void main(String[] args) {
+        int[][] m = new int[][]{{0, 1, 0}, {1, 0, 1}, {1, 1, 1}};
+        func(m);
+        for (int[] ints : m) {
+            for (int j = 0; j < m[0].length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void func(int[][] matrix) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set1 = new HashSet<>();
+        int p = matrix.length;
+        int q = matrix[0].length;
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                if (matrix[i][j] == 0) {
+                    set.add(j);        // 列号
+                    set1.add(i);       // 行号
+                }
+                if (set.size() == q || set1.size() == p) {
+                    break;
+                }
+            }
+        }
+        for (int i : set1) {
+            for (int j = 0; j < q; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        for (int j : set) {
+            for (int i = 0; i < p; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+    }
 }
