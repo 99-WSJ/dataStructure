e07722d0c8b5e4c50dbea52102bc6bc49afefd6e
99-WSJ <sjwang_er@163.com>
2024-09-05 22:13:40 +0800

    9.5

---
+++ Src/src/main/java/examfinal/xiecheng/Main1.java
@@ -0,0 +1,15 @@
+package examfinal.xiecheng;
+
+/**
+ * @program: Src
+ * @description: 第一题
+ * @author: wsj
+ * @create: 2024-09-04 16:03
+ **/
+
+import java.util.Scanner;
+
+
+public class Main1 {
+
+}
---
+++ Src/src/main/java/examfinal/xiecheng/Main3.java
@@ -0,0 +1,17 @@
+package examfinal.xiecheng;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @program: Src
+ * @description: 第一题
+ * @author: wsj
+ * @create: 2024-09-04 16:03
+ **/
+
+public class Main3 {
+}
+
