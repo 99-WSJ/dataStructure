7d2b92b5f58c64d8d413c1be7af37ebc7e286423
99-WSJ <sjwang_er@163.com>
2024-09-21 18:07:35 +0800

    9.21

--- Src/src/main/java/test/basis/FinalDemo.java
+++ Src/src/main/java/test/basis/FinalDemo.java
@@ -3,7 +3,25 @@ package test.basis;
 public class FinalDemo {
 
     public static void main(String[] args) {
-        System.out.println(testFinally());
+        //System.out.println(testFinally());
+        System.out.println(test());
+    }
+    public static String test() {
+        String s = null;
+        try {
+            System.out.println("我是try里面的语句");
+            System.out.println(s.length());
+            return "我是try里的return语句";
+        } catch (NullPointerException e) {
+            System.out.println("我是catch语句里面语句1");
+            return "我是catch语句里面的return语句1";
+        } catch (Exception e) {
+            System.out.println("我是catch语句里面语句2");
+            return "我是catch语句里面的return语句2";
+        } finally {
+            System.out.println("我是finally里面的执行语句");
+            return "我是finally里面的return语句";
+        }
     }
 
     public static int testFinally() {
