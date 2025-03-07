9f5c9c08a396f807a54591b0a4f972faf5d1c839
99-WSJ <sjwang_er@163.com>
2025-03-07 16:38:54 +0800

    3.7

--- Src/src/main/java/structure/test/ArrayStackDemo.java
+++ Src/src/main/java/structure/test/ArrayStackDemo.java
@@ -1,5 +1,4 @@
-package test;
-
+package structure.test;
 import java.util.Scanner;
 
 public class ArrayStackDemo {
@@ -177,4 +176,22 @@ class LinkedStack {
         }
         head.next =  reverseHead.next;
     }
+}
+class HeroNode {
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode next; //指向下一个节点
+    //构造器
+    public HeroNode(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
+    //为了显示方法，我们重新toString
+    @Override
+    public String toString() {
+        return "HeroNode [no=" + no + ", name=" + name + ", nickname=" + nickname + "]";
+    }
+
 }
